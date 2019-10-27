b1b907d0fd638bc8ed4cb68d1bf0188ca6a1e4bb
Natasha S Love <debora@inbox.ru>
2019-10-26 19:21:16 -0500

    first lessons w/ReactNative
    creating screens, View and text elements with style

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
-import LayoutScreen from './src/screens/LayoutScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
 
-const navigator = createStackNavigator({
-  Home: HomeScreen,
-  Layout: LayoutScreen
-});
+const navigator = createStackNavigator(
+  //1 object
+  {
+    //what to show on the screen
+    Home: HomeScreen, 
+    Components: ComponentsScreen
+  },
+  //2 object
+  {
+    //which first component should be displayed
+    initialRouteName: 'Components',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+);
 
 export default createAppContainer(navigator);
---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,41 @@
+//import libraries
+import React from 'react';// all library
+import {Text, StyleSheet, View} from 'react-native';//2 pieces from library
+
+//create function returning JSX
+const ComponentsScreen = () => {
+    //js var - to be inserted into Text
+    const greeting = 'Bye there!';
+    //can do arrays (printing in one line)
+    const arr = ['natasha', ' love', 38];
+    //jsx element inside js var:
+    const jsxel=<Text style={{fontSize:30}}>This is jsx Text element coded inside js var.</Text>
+    
+//taking style from stylesheet component
+//adding View element - to rap the text in one view
+//above- taking property textStyles(30) from obj styles (with dot notation)
+//below - no style applied(standard):
+//{greeting} -js element/var - should be inserted into jsx in {}
+return <View> 
+    <Text style={styles.textStyle}>Getting started with React Native!</Text>
+
+<Text> Hi there! </Text>
+
+<Text>{greeting}</Text>
+<Text style={styles.nameStyle}>{arr}</Text>
+{jsxel}
+<Text style={styles.nameStyle}>This is goodbye message using variable greeting: {greeting}</Text>
+</View>
+};
+// create styles - can create several style object-separate with comma,
+const styles = StyleSheet.create({
+    textStyle:{
+        fontSize: 45
+},
+    nameStyle:{
+        fontSize:20
+    }
+});
+
+//need to export component to be able to use it
+export default ComponentsScreen;
