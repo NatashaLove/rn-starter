0ee4feb639a1792c88c778b3d535bc959ffd489f
Natasha S Love <debora@inbox.ru>
2019-10-30 21:18:55 -0500

    Images
    inserted different images as one component (but diffrent props)

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image } from 'react-native';
+
+const ImageDetail = (props)=>{
+ //console.log(props);
+   //with props - can access different properties of the object (value given in parent)
+    return <View>
+        <Image source={props.imageSource} />
+    <Text>{props.title} </Text>
+
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import {View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = ()=>{
-    return <Text>Image Screen</Text>
+   //making up customized props in ImageDetails -our on names for them
+
+    return (
+        <View>
+        
+       <ImageDetail title="Forest" 
+       imageSource={require('../../assets/forest.jpg')}/>
+       <ImageDetail title="Beach" 
+       imageSource={require('../../assets/beach.jpg')}/>
+       <ImageDetail title="Mountain" 
+       imageSource={require('../../assets/mountain.jpg')}/>
+      <ImageDetail title="Natasha" 
+       imageSource={require('../../assets/natasha.jpg')}/>
+              
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});
