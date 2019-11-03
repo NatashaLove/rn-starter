3115f680b1755e006feb19dd7e9811c4829f4b71
Natasha S Love <debora@inbox.ru>
2019-11-03 14:09:06 -0600

    function useState
    useState func - which watches and reflect the state/value of a variable if it changes

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,11 +1,36 @@
-import React from 'react';
+//useState - function, which allows to watch state
+import React, { useState} from 'react';
 import {View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () =>{
+// incorrect way to declare
+   // let counter =0;
+
+   //correct way to atch state (useState func):
+   const [counter, setCounter]=useState(0);// new state starts from 0(initialized)
+//redestructuring array:, setCounter  - func which gives the value of counter
 
     return (
          <View>
-        <Text>Current Count:</Text>
+        <Button 
+        title="Increase" 
+        onPress={() => {
+            //Don't do this- don't change counter directly!
+            //counter++;
+            //do it with setCounter func:
+            setCounter(counter+1);
+        }}
+        />
+        <Button 
+        title="Decrease" 
+        onPress={()=> {
+            //counter--;
+            setCounter(counter-1);
+            // this func will make the button counter work
+        }}
+        />
+        
+        <Text> Current Count:{counter}</Text>
     </View>
     );
 };
