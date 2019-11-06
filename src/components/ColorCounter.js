d7a5bea7680e5c622c429a06bfdd39b49e590f53
Natasha S Love <debora@inbox.ru>
2019-11-05 19:13:15 -0600

    SquareScreen to increase/decrease color
    working Squarescreen with RGB -3 views in 1 screen to change each component separately.
    use of string template a lot

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -3,7 +3,9 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 
 //({color}) - destructuring  color property from object ColorCounter
 //instead of putting just (props) 
-const ColorCounter = ({color})=>{
+const ColorCounter = ({color, onIncrease, onDecrease})=>{
+    //onIncrease, onDecrease -functions from the SquareScreen(parent)
+    
     return ( 
         //below : {color}- in {}-because it's a template string variable
         //the template string itself is in backticks ``
@@ -11,8 +13,8 @@ const ColorCounter = ({color})=>{
      <View>
          
       <Text> {color}</Text>
-        <Button title={`Increase ${color}`} />
-        <Button title={`Decrease ${color}`} />
+        <Button onPress={()=> onIncrease()} title={`Increase ${color}`} />
+        <Button onPress={()=> onDecrease()} title={`Decrease ${color}`} />
 
      </View>
     )
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -4,15 +4,40 @@ import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = ()=>{
 
-    //3 different areas with ColorCounter view on 1 screen:
-    return <View>
-        <ColorCounter color="Red"/>
-        <ColorCounter color="Blue"/>
-        <ColorCounter color="Green"/>
+//state vars(to watch the state values) are created in the SquareScreen - because it's parent for colorCounter -
+//and defines the objects' props:
+    const[red, setRed]=useState(0);
+    const[green, setGreen]=useState(0);
+    const[blue, setBlue]=useState(0);
+//these state vars will need to be passedd with the help of callback func (onChange)
+//to the child (ColorCounter) - to be modified there (increase/decrease)
 
+    //3 different areas with ColorCounter view on 1 screen:
+    console.log(red);
+    
+    return <View>
+        <ColorCounter 
+        onIncrease={()=> setRed(red + 1)} 
+        onDecrease={()=> setRed(red - 1)} 
+        color="Red"
+        />
+        <ColorCounter 
+        onIncrease={()=> setBlue(blue + 1)} 
+        onDecrease={()=> setBlue(blue - 1)}
+        color="Blue"/>
+        
+        <ColorCounter 
+        onIncrease={()=> setGreen(green + 1)} 
+        onDecrease={()=> setGreen(green - 1)}
+        color="Green"/>
+
+    <View style={{height: 150, width: 150, backgroundColor:`rgb(${red}, ${green}, ${blue})`}}/>
     </View>
 };
-//({}) - destructuring for props
+// the separate <View/> withstye- the square -where we show the changed color
+// {{}} - outside - for javascript expression/inside the actual object literal
+
+//({}) - destructuring for props / definite prop inside
 const styles = StyleSheet.create({});
 
 export default SquareScreen;
